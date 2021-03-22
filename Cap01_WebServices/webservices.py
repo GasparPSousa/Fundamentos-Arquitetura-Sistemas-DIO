9689ffd5a27ecf1b5730976cc6987da554e3204f
GasparPSousa <gasparufrj@gmail.com>
2021-03-22 20:34:54 -0300

    webservices

---
+++ Cap01_WebServices/webservices.py
@@ -0,0 +1,61 @@
+"""
+
+=============================================
+Vantagens e Desvantagens do Web Service
+=============================================
+
+
+======================= Objetivos da Aula =================
+
+
+1 - O que são Serviços Web?
+
+2 - As vantagens de se utilizar Serviços Web.
+
+3 - Tecnologias mais utilizadas para desenvolvimento de Serviços Web.
+
+
+======================= Serviços Web ===========================
+
+
+Serviços Web ou Web Services são soluções para aplicações se comunicarem independente de l
+inguagem, software e hardware utilizados.
+
+Inicialmente Serviços Web foram criados para troca de mensagens utilizando a linguagem XML(Entensible
+Markup Language) sobre o protocolo HTTP sendo identificado por URI( Uniform Reource Identifier)
+
+Podemos dizer que Serviços Web são APIs que se comunicam por meio de redes sobre o protocolo HTTP.
+
+
+
+======================= Vantagens ===============================
+
+
+- linguagem comum
+
+- Integração
+
+- Reutilização d Implementação
+
+- Segurança
+
+- Custos
+
+
+======================== Principais Tecnologias =========================
+
+- SOAP (Arquitetura utilizada em WebServices - Sempre usado com XML)
+
+- REST (Arquitetura utilizada em WebServices - Mais atrelado ao JSON, mas pode ser usado XML)
+
+- XML (Linguagem de Marcação)
+
+- JSON (Linguagem de Marcação)
+
+
+
+
+
+
+"""
+
