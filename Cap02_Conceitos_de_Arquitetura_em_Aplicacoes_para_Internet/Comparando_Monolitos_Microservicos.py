b255856741e24563a7e50862452f39fd19e501e1
GasparPSousa <gasparufrj@gmail.com>
2021-03-23 14:32:54 -0300

    Comparando os modelos Monolitos e Microserviços

---
+++ Cap02_Conceitos_de_Arquitetura_em_Aplicacoes_para_Internet/Comparando_Monolitos_Microservicos.py
@@ -0,0 +1,79 @@
+""""
+=====================================================================================
+                Comparativo entre os Tipos de Arquiteruta
+=====================================================================================
+
+
+===================== Pros e Contras - Monolito =====================================
+
+==== Pros ====
+- Baixa Complexidade
+- Monitoramento Simplificado
+
+==== Contra ====
+- Stack Única
+- Compartilhamento de Recursos
+- Acoplamento
+- Mais complexo a escalabilidade
+
+
+
+==================== Pros e Contras - Microserviços #1 ===============================
+
+==== Pros ====
+- Stack Dinâmica
+- Simples Escalabilidade
+
+==== Contras ====
+- Acoplamento
+- Monitoramento mais complexo
+- Provisionamento mais complexo
+
+
+==================== Pros e Contras - Microserviços #2 ===============================
+
+==== Pros ====
+- Stack Dinâmica
+- Simples Escalabilidade
+- Desacoplamento
+
+==== Contras ====
+- Monitoramento mais complexo
+- Provisionamento mais complexo
+- Plataforma inteira depende do Message Broker (Single Point of Failure)
+
+
+==================== Pros e Contras - Microserviços #3 ===============================
+
+==== Pros ====
+- Stack Dinâmica
+- Simples Escalabilidade
+- Desacoplamento
+- Menor Complexidade
+
+==== Contras ====
+- Provisionamento mais complexo
+- Plataforma inteira depende do pipeline (Single Point of Failure)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
--- Cap02_Conceitos_de_Arquitetura_em_Aplicacoes_para_Internet/README.md
+++ Cap02_Conceitos_de_Arquitetura_em_Aplicacoes_para_Internet/README.md
@@ -1,4 +1,5 @@
 # Cap02 - Conceitos de Arquitetura em Aplicações para Internet
 
 ### Aula 1 - Introdução a Arquitetura de Sistemas
+### Aula 2 - Comparando os modelos Monolito e Microserviços
 
