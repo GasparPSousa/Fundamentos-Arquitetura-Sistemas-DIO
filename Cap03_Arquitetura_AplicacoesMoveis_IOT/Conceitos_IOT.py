6c7528e9fea3be21f409c6a7af8a02c2ea1e505b
GasparPSousa <gasparufrj@gmail.com>
2021-03-23 16:32:44 -0300

    Conceitos da IoT

---
+++ Cap03_Arquitetura_AplicacoesMoveis_IOT/Conceitos_IOT.py
@@ -0,0 +1,127 @@
+"""
+===================================================================================
+                                        IOT
+===================================================================================
+
+
+
+=========================== Objetivos da Aula =====================================
+
+1 - Introdução a Internet das Coisas
+
+2 - Arquitetura da Internet das Coisas
+
+3 - Estudo de Caso
+
+
+============================ Requisitos Básicos ===================================
+
+- Cloud Computing
+
+- Protocolo de comunicação de rede
+
+- Processamento e Armazenamento de dados
+
+
+
+
+======================= Parte 1: Introdução a IOT ==================================
+
+ARPANET - visava conectar computadores.
+
+Virou a rede mundial de computadores.
+
+Com o advento da internet sugiu os aplicativos.
+
+O que todos esses aplicativos estão fazendo é utilizando a internet para conectar PESSOAS.
+
+A internet é uma rede de pessoas conectadas trocando informações constantemente.
+
+É aí que surge a IOT, quando COISAS usa a internet para se conectar e trocar dados, enviar informações.
+
+
+============================= Por que conectar as Coisas?? ===========================
+
+- Embutir sensores em objetos do dia-a-dia.
+
+- Coletar dados dos sensores.
+
+- Usar o dado para tomar decisão, resolver problemas e simplificar as coisas do dia-a-dia.
+
+
+============================ Conceitos Básicos de IoT ===============================
+
+
+THINGS                                 CLOUD                              INTELLIGENCE
+
+Onde queremos coletar dados        Onde queremos armazenar dados         Ter Vantagem Competitiva
+
+
+
+============================= Exemplos de IOT =========================================
+
+
+- Smart Building  - É a capacidade de você conseguir coletar dados desses componentes e utilizar esses dados
+de forma inteligente para poder melhor a segurança do prédia, ou melhorar o sistema de prevenção de incêndios
+ou garantir que só terá acesso a determinadas áreas pessoas autorizadas.
+
+- Smart Home
+
+- Wearables  - Componentes que colocamos no nosso corpo.
+
+- Agricultura
+
+- Smart Transportation
+
+- RFID Supply Chain - Aqui é onde o IOT nasceu, a tecnologia de RFID de etiquetas e scanners de radiofrequência.
+
+- Energy Efficiency
+
+
+==================================== Computaão Ambígua =====================================
+
+"A computação ambígua é a terceira onda de computação que está apenas começando.
+
+Primeiro tivemos os mainframes compartilhados por várias pessoas.
+
+Estamos na Era da computação pessoal com pessoas e máquinas estranhando umas as outras.
+
+A seguir vem a computação umbígua, a Era da tecnologia "calma", quando a tecnologia recua para o fundo
+de nossas vidas.
+
+As tecnologias mais importantes são aquelas que desaparecem. Elas se integram à vida do dia a dia,
+ao nosso cotidiano até serem indistinguíveis deles."
+
+Mark Weises
+
+
+================================== Desafios da IoT ==========================================
+
+1 - Privacidade e Segurança
+
+2 - Quantidade exponencial de dispositivos conectados a rede.
+
+3 - Ser capaz de processar e armazenar uma enorme quantidade de informações.
+
+4 - Gerar valor a partir dos dados coletados.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
---
+++ Cap03_Arquitetura_AplicacoesMoveis_IOT/README.md
@@ -0,0 +1,3 @@
+# Cap03 - A Arquitetura de Aplicações Móveis e IoT
+
+### Aula 1 - Conceitos de IoT
