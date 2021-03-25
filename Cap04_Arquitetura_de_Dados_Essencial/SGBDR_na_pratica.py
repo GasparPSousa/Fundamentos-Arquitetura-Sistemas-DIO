10848d0d2403f0884c0c81277c363882ca4cef65
GasparPSousa <gasparufrj@gmail.com>
2021-03-25 11:47:24 -0300

    SGDBR na pratica

---
+++ Cap04_Arquitetura_de_Dados_Essencial/SGBDR_na_pratica.py
@@ -0,0 +1,42 @@
+"""
+================================================================================
+                                SGBDR na Prática
+================================================================================
+
+
+================================== SGBDs - R ==================================
+
+COMERCIAIS
+
+Oracle
+Microsoft SQL Server
+IBM DB2
+
+
+OPEN SOURCE
+
+PostgreSQL
+MySQL
+SQLite
+
+
+=================================== Instalando o Postgres =====================
+Ubuntu(Debian)
+https://www.postgresql.org/download/linux/ubuntu/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
